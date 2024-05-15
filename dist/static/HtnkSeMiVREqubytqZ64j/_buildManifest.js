9b68c7b373c6229d9637bddd3084027cb2c20b14
Hilman Nafi'in <hilmannafiin@Hilmans-MacBook-Air.local>
2024-05-16 04:56:02 +0700

    test

---
+++ dist/static/HtnkSeMiVREqubytqZ64j/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/268-630d4c1a542f9553.js","static/chunks/pages/index-a52c8c7bfea57a28.js"],"/_error":["static/chunks/pages/_error-3c9a37b9888d2d4e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
